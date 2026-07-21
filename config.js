dbe842ce6bb36e6ac0474ac43341a731dd4c00dd
e <e@e>
2026-07-20 19:21:19 -0500

    conectar despliegue corregido

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   API_URL:
-    "https://script.google.com/macros/s/AKfycbxlACNyw6vGfETsIgv6EU7mstHoQ9lQAeAsp_9vXSBr3UX8h78NInrGRXotWGhs-_6O/exec",
+    "https://script.google.com/macros/s/AKfycbxRPeKs31UxJlInl72AwGiD8yqaAvcs4HknG-j3YtJlseS4P0AEMnWS7OCeWQ3IiJbB/exec",
   DEMO_MODE: false,
 };
